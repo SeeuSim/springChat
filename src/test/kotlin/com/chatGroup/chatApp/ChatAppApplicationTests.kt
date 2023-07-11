b1f0b87ef8cbbd1d828ee972ff9640acf405d5f5
SeeuSim <seeusimong@gmail.com>
2023-07-11 23:00:42 +0800

    Initial Commit

---
+++ src/test/kotlin/com/chatGroup/chatApp/ChatAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chatGroup.chatApp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
